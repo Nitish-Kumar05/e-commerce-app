fb51e661278d573d1993baa75aa15b519da5eb48
Nitish-Kumar05 <mvnitishkumar@gmail.com>
2023-09-02 10:20:04 +0530

    adding and removing products in cart can be done now

--- src/pages/cart/CartItem.jsx
+++ src/pages/cart/CartItem.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../../context/ShopContext'
 
 const CartItem = (props) => {
 
     const { id, productName, price, productImage } = props.data
+    const { cartItems, addToCart, removeFromCart } = useContext(ShopContext)
 
     return (
         <div className='cartItem' >
@@ -14,6 +16,11 @@ const CartItem = (props) => {
                 <p>
                     ${price}*
                 </p>
+                <div className='countHandler' >
+                    <button onClick={() => removeFromCart(id)} > - </button>
+                    <input value={cartItems[id]} />
+                    <button onClick={() => addToCart(id)} > + </button>
+                </div>
             </div>
         </div>
     )
